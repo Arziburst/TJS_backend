4e354cd380cd18af1885351af62a2983feb91073
Belartale <belartale@gmail.com>
2023-10-31 14:05:08 +0200

    fix chageStatus (liqPay)

--- source/domains/orders/handlers/changeStatus.ts
+++ source/domains/orders/handlers/changeStatus.ts
@@ -30,7 +30,7 @@ type ReduceResult = {
 }
 
 
-export const changeStatus = async (req: any, res: Response) => {
+export const changeStatus = async (req: any, res: Response, next: Function) => {
     debug(`${req.method} — ${req.originalUrl}`);
 
     try {
@@ -53,52 +53,56 @@ export const changeStatus = async (req: any, res: Response) => {
             if (dataThen.status === 'success') {
                 // data = dataThen;
                 const foundOrder = await Orders.findById(id);
-                if (foundOrder) {
-                    const updatedOrder = await Orders.findOneAndUpdate(foundOrder._id, {
-                        ...foundOrder,
-                        statusPayment: 'paid',
-                    });
 
-                    if (updatedOrder) {
-                        const { total, orderedProducts } = updatedOrder.orderedProducts.reduce((
-                            acc, orderedProduct,
-                        ) => {
-                            return {
-                                orderedProducts: [
-                                    ...acc.orderedProducts,
-                                    {
-                                        pid:   orderedProduct.pid,
-                                        image: orderedProduct.image,
-                                        price: orderedProduct.price,
-                                    },
-                                ],
-                                total: acc.total + orderedProduct.price,
-                            };
-                        }, { total: 0, orderedProducts: [] } as ReduceResult);
-
-                        // Telegram notification
-                        const { created, phone, comment, _id } = updatedOrder;
-                        const parsedCreated = moment(created).locale('ru')
-                            .format('MMMM Do YYYY, h:mm:ss a');
-
-                        await axios.post(`${TELEGRAM_API_URL}/sendMessage`, {
-                            chat_id:    TELEGRAM_GROUP_ID,
-                            parse_mode: 'HTML',
-                            text:       `Заказ: <a href ='http://tjstore.pp.ua/orders/${_id}'>${_id}</a>\nНа сумму: ${total} грн.\nКол-во: ${orderedProducts.length}\nСоздан: ${parsedCreated}\n${comment && `Комментарий: ${comment}\n`}Телефон: <a href='tel:${phone}'>${phone}</a>.`,
-                        });
-
-                        res.status(302).redirect(process.env.PUBLIC_URL + '/payment-success' || '');
-                    }
+                if (!foundOrder) {
+                    throw new Error('Order not found');
                 }
-            } else {
-                await Orders.findOneAndRemove(dataThen.order_id);
-                res.status(400).redirect(process.env.PUBLIC_URL + '/payment-fail' || '');
+                const updatedOrder = await Orders.findOneAndUpdate(foundOrder._id, {
+                    ...foundOrder,
+                    statusPayment: 'paid',
+                });
+
+                if (!updatedOrder) {
+                    throw new Error('Order not found');
+                }
+
+                const { total, orderedProducts } = updatedOrder.orderedProducts.reduce((
+                    acc, orderedProduct,
+                ) => {
+                    return {
+                        orderedProducts: [
+                            ...acc.orderedProducts,
+                            {
+                                pid:   orderedProduct.pid,
+                                image: orderedProduct.image,
+                                price: orderedProduct.price,
+                            },
+                        ],
+                        total: acc.total + orderedProduct.price,
+                    };
+                }, { total: 0, orderedProducts: [] } as ReduceResult);
+
+                // Telegram notification
+                const { created, phone, comment, _id } = updatedOrder;
+                const parsedCreated = moment(created).locale('ru')
+                    .format('MMMM Do YYYY, h:mm:ss a');
+
+                await axios.post(`${TELEGRAM_API_URL}/sendMessage`, {
+                    chat_id:    TELEGRAM_GROUP_ID,
+                    parse_mode: 'HTML',
+                    text:       `Заказ: <a href ='http://tjstore.pp.ua/orders/${_id}'>${_id}</a>\nНа сумму: ${total} грн.\nКол-во: ${orderedProducts.length}\nСоздан: ${parsedCreated}\n${comment && `Комментарий: ${comment}\n`}Телефон: <a href='tel:${phone}'>${phone}</a>.`,
+                });
+
+                res.redirect(process.env.PUBLIC_URL + '/payment-success' || '');
+
+                return dataThen;
             }
+            await Orders.findOneAndRemove(dataThen.order_id);
+
+            res.redirect(process.env.PUBLIC_URL + '/payment-fail' || '');
 
             return dataThen;
         });
-
-        res.status(400).redirect(process.env.PUBLIC_URL + '/payment-fail' || '');
     } catch (error: any) {
         res.status(400).json({ message: error.message })
             .redirect(process.env.PUBLIC_URL + '/payment-fail' || '');
